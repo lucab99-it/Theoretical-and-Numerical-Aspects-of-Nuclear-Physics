e8cbab731b7aeaa26c5a900f4800161dfe23a6c8
Luca Brunelli <72265488+lucab99-it@users.noreply.github.com>
2023-08-29 12:59:55 +0200

    Add files via upload

--- Cubic_Harmonic_Oscillator_MC.py
+++ Cubic_Harmonic_Oscillator_MC.py
@@ -22,11 +22,11 @@ g = np.zeros((N_cf,N))
 G3, G3_errors= MCAverage(g,N,epsilon,N_cor,N_cf,f=getG3)# MC average and statistical error of G3
 
 #Compute the energy gap at time tn for each n and the corresponding statistical error
-dE3,deltaE3, deltaE3_graph = DeltaE(G3,G3_errors,N_cf) #energy gap
+dE3,deltaE3 = DeltaE(G3,G3_errors,N_cf) #energy gap
 
 
 fig = plt.figure()
-l1 = plt.errorbar(t,dE3, yerr= deltaE3_graph, ecolor = 'r', capsize=5, label=r'$\Delta E (t)$', marker = '.', linestyle = 'none') #numerical value
+l1 = plt.errorbar(t,dE3, yerr= deltaE3, ecolor = 'r', capsize=5, label=r'$\Delta E (t)$', marker = '.', linestyle = 'none') #numerical value
 l2 = plt.plot(t,np.ones(N), label = r'$\Delta E(\infty)$', linestyle = 'dotted') #exact asymptotic value
 plt.axis([-0.1,3,0,2])
 plt.xlabel('t')

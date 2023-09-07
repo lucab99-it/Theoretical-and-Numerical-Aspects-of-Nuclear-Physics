59b07cf8814d87b5f4d7b3f16779fcd966dbaa69
Luca Brunelli <72265488+lucab99-it@users.noreply.github.com>
2023-09-07 19:31:28 +0200

    Fixed Harmonic_Oscillator_Modified and added descriptive comments

--- Harmonic_Oscillator_Solutions.py
+++ Harmonic_Oscillator_Solutions.py
@@ -7,10 +7,13 @@ N = 20 #total lattice sites
 T = a*N #total time
 Npoints = 100
 t = np.linspace(0, T, N) #time axis
-Option = 'Improved'
-w0=np.linspace(0,4,Npoints) #known frequency of the oscillator
+improved = True
+w0=np.linspace(0.2,4,Npoints) #known frequency of the oscillator
 
-
+#The following function defines the matrix associated with the discretization of the
+#differential operator of the equations of motion. The discretization is done based on the
+#central difference formula of the second derivative at first order.
+#It takes as input the oscillator frequency w0 and returns a NxN array M
 def SystemMatrix(w0):
     M = np.zeros((N,N))
     for i in range(N):
@@ -23,6 +26,11 @@ def SystemMatrix(w0):
             M[ip][i] = 1
     return M
 
+
+#The following function defines the matrix associated with the discretization of the
+#differential operator of the equations of motion. The discretization is done based on the
+#improved formula for the second derivative (second order formula).
+#It takes as input the oscillator frequency w0 and returns a NxN array M
 def SystemMatrixImproved(w0):
     M = np.zeros((N,N))
     for j in range(N):
@@ -41,7 +49,9 @@ def SystemMatrixImproved(w0):
             M[jp2][j] = -1/12
     return M
 
-
+#The following function solves the linear system Mx = h where M is the matrix of the unimproved second derivative,
+# and h is a length-N array, h = [-x0,0...,0,xN] where x0 and xN are passed as inputs, together with the oscillator frequency w0.
+#It returns an array x solution to the aforementioned system.
 def Solve(w0,x0, xN):
     h = np.zeros(N)
     h[0] = -x0
@@ -50,6 +60,10 @@ def Solve(w0,x0, xN):
     x = np.linalg.solve(M,h)
     return x
 
+#The following function solves the linear system Mx = h where M is the matrix of the improved second derivative and
+# h is a length-N array, h = [(1/12 x1-4/3 x0),(1/12)x0,0...,0,1/12 xN,(1/12 xNm1-4/3 xN)]
+# where x0,x1,xNm1 and xN are passed as inputs, together with the oscillator frequency w0.
+#It returns an array x solution to the aforementioned system.
 def SolveImproved(w0,x0,x1,xNm1,xN):
     h = np.zeros(N)
     h[0]=(1/12)*x1-(4/3)*x0
@@ -60,36 +74,41 @@ def SolveImproved(w0,x0,x1,xNm1,xN):
     x = np.linalg.solve(M,h)
     return x
 
+#Parametric custom-made exponential function for fit purposes
 def exp(x,A,w):
     return A*np.exp(-w*x)
 
-
-def PlotSolution(Option, w0, w_exp, w_sol):
+#The following function plots the solution in a graph containing both the numerical solution w_sol
+#and its expected behaviour w_exp. Alongside these, it takes as input a boolean switch improved, which selects
+#whether to graph an improved or uninproved expectation for w, and the array of frequencies w0
+def PlotSolution(improved, w0, w_exp, w_sol):
     fig = plt.figure()
-    if Option == 'Std':
-        l1=plt.plot(w0,w_exp,label = r'$\omega_0\sqrt{1-\frac{(a\omega_0)^2}{12}}$', linestyle = 'dotted')
-        l2 = plt.plot(w0,w_sol, label = 'Numerical solution', linestyle='none', marker = '.')
-
-    if Option == 'Improved':
+    if improved:
         l1=plt.plot(w0,w_exp,label = r'$\omega_0\sqrt{1+\frac{(a\omega_0)^4}{90}}$', linestyle = 'dotted')
-        l2 = plt.plot(w0,w_sol, label = 'Numerical solution', linestyle='none', marker = '.')
+    else:
+        l1=plt.plot(w0,w_exp,label = r'$\omega_0\sqrt{1-\frac{(a\omega_0)^2}{12}}$', linestyle = 'dotted')
 
+    l2 = plt.plot(w0,w_sol, label = 'Numerical solution', linestyle='none', marker = '.')
     plt.xlabel(r'$\omega_0$')
     plt.ylabel('$\omega$')
     plt.legend(loc='lower right')
     plt.show()
 
-
-def HarmonicOscillatorSolution(w0,Option, Npoints=100):
+#The following function solves the equations of motion of a Harmonic Oscillator
+#with frequency varying as in the array w0 passed as input. Alongside this, it takes as inputs
+#a boolean switch improved, and an optional integer argument Npoints which lets the user select the number of
+#points in the final graph.
+#Finally, it plots the numerical solution with the expected behaviour as a function of w0.
+def HarmonicOscillatorSolution(w0,improved, Npoints=100):
     x_sol = np.zeros((Npoints,N))
     w_sol = np.zeros(Npoints)
     w_exp = np.zeros(Npoints)
-    x0 = np.exp(0)
+    x0 = np.exp(0) #x0 = e^(-w*0), initial point of the lattice
     for k in range(Npoints):
-        xN = np.exp(-w0[k]*T)
-        if Option == 'Improved':
-            x1 = np.exp(w0[k]*a)
-            xNm1 = np.exp(-w0[k]*(T-a))
+        xN = np.exp(-w0[k]*T)#xN = e^(-w*T) final point of the lattice
+        if improved:
+            x1 = np.exp(w0[k]*a)#for improved case we consider x1= e^(-w*(-a))
+            xNm1 = np.exp(-w0[k]*(T-a))# xNm1 = e^(-w*(T-a))
             x_sol[k] = SolveImproved(w0[k],x0,x1,xNm1,xN)
             w_exp[k] = w0[k]*np.sqrt(1+((a*w0[k])**4)/90)
 
@@ -97,10 +116,11 @@ def HarmonicOscillatorSolution(w0,Option, Npoints=100):
             x_sol[k] = Solve(w0[k], x0, xN)
             w_exp[k] = w0[k]*np.sqrt(1-((a*w0[k])**2)/12)
 
+#Fitting the solution with the exponential function to retrieve w_sol
         optpar, cov = opt.curve_fit(exp, t, x_sol[k], p0 = (0.1, w0[k]))
         w_sol[k] = optpar[1]
-    PlotSolution(Option,w0, w_exp, w_sol)
+    PlotSolution(improved,w0, w_exp, w_sol)
 
 
-
-HarmonicOscillatorSolution(w0,Option, Npoints = Npoints)
+#Main body:
+HarmonicOscillatorSolution(w0,improved, Npoints = Npoints)
--- Harmonic_Oscillator_Solutions_Modified.py
+++ Harmonic_Oscillator_Solutions_Modified.py
@@ -1,32 +1,75 @@
 import numpy as np
-from scipy.optimize import fsolve
+from scipy import optimize as opt
 import matplotlib.pyplot as plt
 
 a=0.5 #lattice spacing
 N = 20 #total lattice sites
-w0=np.linspace(0,4,100) #known frequency of the oscillator
-w_sol = np.zeros(100)
-w_exp = np.zeros(100)
-w_guess = 1
+T = a*N #total time
+Npoints = 100
+t = np.linspace(0, T, N) #time axis
+w0=np.linspace(0.5,4,Npoints)
 
-#Order-a^2 second derivative: central method numerical differentiation.
-def D(x,j):
-    jp = (j+1)%N
-    jm = (j-1)%N
-    return (x[jp]-2*x[j]+x[jm])/(a**2)
+#The following function defines the matrix associated with the discretization of the
+#differential operator of the second derivative. The discretization is done based on the
+#central difference formula at first order with transformed anharmonic potential.
+# It takes as input the oscillator frequency w0 and returns a NxN array M
+def SystemMatrixTransformed(w0):
+    M = np.zeros((N, N))
+    for j in range(N):
+        jm = (j-1)
+        jp = (j+1)
+        if jm >= 0:
+            M[jm][j] = 1
+        M[j][j] = -(2 + (a*w0)**2*(1+(a*w0)**2/12))
+        if jp < N:
+            M[jp][j] = 1
+    return M
 
-def f_mod(w,w0,j):
-    x=[np.exp(-a*k*w) for k in range(N)]
-    return D(x,j) - w0**2*(1+(a*w0)**2/12)*x[j]
+#The following function solves the linear system Mx = h where M is the matrix of the transformed discretization of the eqs,
+# and h is a length-N array, h = [-x0,0...,0,xN] where x0 and xN are passed as inputs, together with the oscillator frequency w0.
+#It returns an array x solution to the aforementioned system.
+def SolveTransformed(w0,x0, xN):
+    h = np.zeros(N)
+    h[0] = -x0
+    h[-1] = xN
+    M = SystemMatrixTransformed(w0)
+    x = np.linalg.solve(M,h)
+    return x
 
-for n in range(100):
-    w_sol[n] = fsolve(f_mod,w_guess, args=(w0[n], 2))
-    w_exp[n] = w0[n]*np.sqrt(1+(a*w0[n])**4/90)
 
-fig = plt.figure()
-l1=plt.plot(w0,w_exp,label = r'$\omega_0 \sqrt{1+\frac{(a\omega_0)^4}{90}}$', linestyle = 'dotted')
-l2 = plt.plot(w0,w_sol, label = 'Numerical solution', linestyle='none', marker = '.')
-plt.xlabel(r'$\omega_0$')
-plt.ylabel('$\omega$')
-plt.legend(loc='lower right')
-plt.show()
+#Parametric custom-made exponential function for fit purposes
+def exp(x,A,w):
+    return A*np.exp(-w*x)
+
+#The following function plots the solution in a graph containing both the numerical solution w_sol
+#and its expected behaviour w_exp. Alongside these, it takes as input the array of known frequencies w0
+def PlotSolution(w0, w_exp, w_sol):
+    fig = plt.figure()
+    l1=plt.plot(w0,w_exp,label = r'$\omega_0\sqrt{1-\frac{(a\omega_0)^4}{360}}$', linestyle = 'dotted')
+    l2 = plt.plot(w0,w_sol, label = 'Numerical solution', linestyle='none', marker = '.')
+    plt.xlabel(r'$\omega_0$')
+    plt.ylabel('$\omega$')
+    plt.legend(loc='lower right')
+    plt.show()
+
+#The following function solves the equations of motion of a Harmonic Oscillator
+#with frequency varying as in the array w0, passed as input, with the transformed anharmonic potential.
+# It also has an optional argument Npoints which lets the user select the number of
+#points in the final graph.
+#It finally plots the numerical solution with the expected behaviour as a function of w0.
+def HarmonicOscillatorSolution(w0, Npoints=100):
+    x_sol = np.zeros((Npoints,N))
+    w_sol = np.zeros(Npoints)
+    w_exp = np.zeros(Npoints)
+    x0 = np.exp(0)
+    for k in range(Npoints):
+        xN = np.exp(-w0[k]*T)
+        x_sol[k] = SolveTransformed(w0[k], x0, xN)
+        w_exp[k] = w0[k]*np.sqrt(1-((a*w0[k])**4)/360)
+#Fitting the solution with the exponential function to retrieve w_sol
+        optpar, cov = opt.curve_fit(exp, t, x_sol[k], p0 = (0.1, w0[k]))
+        w_sol[k] = optpar[1]
+    PlotSolution(w0, w_exp, w_sol)
+
+#Main Body
+HarmonicOscillatorSolution(w0)
